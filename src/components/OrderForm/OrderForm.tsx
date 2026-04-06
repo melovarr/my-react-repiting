87e7b140ce62d285bb50179a0b85505b35a977ba
Serhii Zhelobkov <meeelnik@gmail.com>
2026-04-06 12:38:11 +0300

    added checkbox group and select group

--- src/components/OrderForm/OrderForm.tsx
+++ src/components/OrderForm/OrderForm.tsx
@@ -6,10 +6,15 @@ interface OrderFormProps {
 }
 
 export default function OrderForm({ onSubmit }: OrderFormProps) {
+  const selectId = useId();
   const handleSubmit = (formData: FormData) => {
     const username = formData.get('username') as string;
     const delivery = formData.get('delivery') as string;
     console.log('Delivery', delivery);
+    const restrictions = formData.getAll('restrictions') as string[];
+    console.log('Dietary restrictions:', restrictions);
+    const deliveryTime = formData.get('deliveryTime') as string;
+    console.log('Preferred delivery time:', deliveryTime);
 
     onSubmit(username);
   };
@@ -46,6 +51,32 @@ export default function OrderForm({ onSubmit }: OrderFormProps) {
           Drone delivery
         </label>
       </fieldset>
+      <fieldset>
+        <legend>Dietary restrictions:</legend>
+        <label>
+          <input type="checkbox" name="restrictions" value="vegetarian" />
+          Vegetarian
+        </label>
+        <label>
+          <input type="checkbox" name="restrictions" value="vegan" />
+          Vegan
+        </label>
+        <label>
+          <input type="checkbox" name="restrictions" value="gluten-free" />
+          Gluten-free
+        </label>
+        <label>
+          <input type="checkbox" name="restrictions" value="nut-free" />
+          Nut-free
+        </label>
+      </fieldset>
+      <label htmlFor={selectId}>Preferred delivery time</label>
+      <select name="deliveryTime" id={selectId} defaultValue="">
+        <option value="">-- Choose delivery time --</option>
+        <option value="morning">Morning (8:00 AM - 12:00 PM)</option>
+        <option value="afternoon">Afternoon (12:00 PM - 4:00 PM)</option>
+        <option value="evening">Evening (4:00 PM - 8:00 PM)</option>
+      </select>
       <button type="submit" className={styles.btn}>
         Place order
       </button>
